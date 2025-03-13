6b2341cf92536c8da360e755374263889c8d3e19
feafania <173483105+feafania@users.noreply.github.com>
2025-03-13 22:53:30 +0000

    fix: update interface in types/index.ts

--- news-api/src/types/index.ts
+++ news-api/src/types/index.ts
@@ -1,14 +1,44 @@
+export enum Methods {
+    Get = 'GET',
+    Post = 'POST',
+    Put = 'PUT',
+    Delete = 'DELETE',
+}
+export type StringRecord = Record<string, string>;
+
+export type Callback<T = unknown> = (data: T) => void;
+
+type Result = 'ok' | 'error';
+
 export interface NewsSource {
     id: string;
     name: string;
     description: string;
     url: string;
-    category: string; // The type of news to expect from this news source.
+    category: string;
     language: string;
-    country: string; // The country this news source is based in (and primarily writes about).
+    country: string;
+}
+export interface NewsArticle {
+    source: SourceObject;
+    author?: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage?: string;
+    publishedAt: string; // The date and time that the article was published, in UTC (+000)
+    content?: string; // The unformatted content of the article, where available. This is truncated to 200 chars
+}
+export type SourceObject = {
+    id: string | null;
+    name: string;
+};
+
+export interface ApiResponse<T> {
+    status: Result;
+    sources?: ReadonlyArray<T>;
+    articles?: ReadonlyArray<T>;
 }
 
-export interface NewsApiResponse {
-    status: string; // If the request was successful or not. Options: ok, error. In the case of error a code and message property will be populated.
-    sources: NewsSource[];
-}
+export type ArticlePreview = Pick<NewsArticle, 'title' | 'url' | 'description'>;
+export type PartialArticle = Partial<NewsArticle>;
