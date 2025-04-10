203e486f14132599583454f17dcc434fb06ac54c
feafania <173483105+feafania@users.noreply.github.com>
2025-04-10 21:23:08 +0100

    feat: add dynamic view rendering in init-app

--- async-race/src/init-app.ts
+++ async-race/src/init-app.ts
@@ -1,8 +1,29 @@
+import { createHeader } from "./views/common/header";
+import { createFooter } from "./views/common/footer";
+import { createMain } from "./views/common/main";
+import { renderGarageView } from "./views/pages/garage";
+import { renderWinnersView } from "./views/pages/winners";
+import "./styles/global.css";
+import { PageType } from "./types/enum";
+
 export default function initApp(root: HTMLElement) {
+  root.id = "root";
   root.innerHTML = "";
-  const header = document.createElement("header");
-  header.textContent = "Async race";
-  const main = document.createElement("main");
-  const footer = document.createElement("footer");
+
+  const header = createHeader();
+
+  const main = createMain((page: PageType) => {
+    const container = document.querySelector("#view-container");
+    if (!container) return;
+    container.innerHTML = "";
+    if (page === "garage") {
+      container.append(renderGarageView());
+    } else if (page === "winners") {
+      container.append(renderWinnersView());
+    }
+  });
+
+  const footer = createFooter();
+
   root.append(header, main, footer);
 }
