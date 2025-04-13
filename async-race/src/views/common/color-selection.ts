2b20d5aa3a0eadd99a30ea1a5c13e40281a1be2c
feafania <173483105+feafania@users.noreply.github.com>
2025-04-14 00:40:02 +0100

    feat: add color-selection

---
+++ async-race/src/views/common/color-selection.ts
@@ -0,0 +1,39 @@
+import "@melloware/coloris/dist/coloris.css";
+import Coloris from "@melloware/coloris";
+import "../../styles/color-picker.css";
+
+import { ControlConfig } from "../../types/interfaces";
+import { DEFAULT_CAR_COLOR } from "../../constants";
+import { getState, saveState } from "../../utils/helpers";
+
+export function createColorPicker(object: ControlConfig): HTMLInputElement {
+  Coloris.init();
+  Coloris({ el: "#coloris" });
+
+  const colorPicker = document.createElement("input");
+  colorPicker.type = "text";
+  colorPicker.className = object.class ? `${object.class} coloris` : "coloris";
+
+  colorPicker.dataset.coloris = "";
+
+  colorPicker.id = object.id ?? `color-picker-${Date.now()}`;
+
+  if (object.class) {
+    colorPicker.className = object.class;
+  }
+
+  colorPicker.disabled = object.disabled || false;
+
+  colorPicker.value =
+    object.name || getState(colorPicker.id) || DEFAULT_CAR_COLOR;
+  colorPicker.readOnly = true;
+  colorPicker.style.backgroundColor = colorPicker.value;
+
+  colorPicker.addEventListener("input", () => {
+    colorPicker.style.backgroundColor = colorPicker.value;
+    saveState({ key: colorPicker.id, value: colorPicker.value });
+  });
+
+  Coloris.close();
+  return colorPicker;
+}
