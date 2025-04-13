4cf07aa00387b3e54a1fe0a914359ed79fcf0153
feafania <173483105+feafania@users.noreply.github.com>
2025-04-14 00:30:49 +0100

    refactor: header style and move buttons from main

--- async-race/src/views/common/header.ts
+++ async-race/src/views/common/header.ts
@@ -1,12 +1,32 @@
 import "../../styles/header.css";
+import { capitalizeString } from "../../utils/helpers";
+import { PageType } from "../../types/enum";
 
-export function createHeader(): HTMLElement {
+import { createButton } from "./button";
+
+export function createHeader(
+  onNavigate: (pageName: string) => void,
+): HTMLElement {
   const header = document.createElement("header");
   header.className = "header";
 
   const title = document.createElement("h1");
   title.textContent = "Async Race";
-  header.append(title);
+  title.className = "title";
+
+  const controls = document.createElement("div");
+  controls.className = "page-controls";
+
+  const garageButton = createButton(
+    { name: capitalizeString(PageType.Garage), class: "view-button" },
+    () => onNavigate(PageType.Garage),
+  );
+  const winnersButton = createButton(
+    { name: capitalizeString(PageType.Winners), class: "view-button" },
+    () => onNavigate(PageType.Winners),
+  );
+  controls.append(garageButton, winnersButton);
+  header.append(title, controls);
 
   return header;
 }
