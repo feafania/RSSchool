af07d95106416cf31ffd10929df6c178447b930a
feafania <173483105+feafania@users.noreply.github.com>
2025-04-14 23:35:16 +0100

    feat: add api settings

--- async-race/src/constants/index.ts
+++ async-race/src/constants/index.ts
@@ -3,3 +3,13 @@ export const TOTAL_RANDOM_CARS = 100;
 
 export const DEFAULT_CAR_COLOR = "#ffff00";
 export const VIEW_CONTAINER_NAME = "view-container";
+
+export const API_SETTINGS = {
+  PORT: 3000,
+  PATH: {
+    GARAGE: "garage",
+    ENGINE: "engine",
+    WINNERS: "winners",
+  },
+  BASE_URL: "http://127.0.0.1",
+};
