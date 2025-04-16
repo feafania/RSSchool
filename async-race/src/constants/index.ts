86a4e4405beee6738173917896a230b7c33b7a4f
feafania <173483105+feafania@users.noreply.github.com>
2025-04-16 15:11:54 +0100

    feat: add http status

--- async-race/src/constants/index.ts
+++ async-race/src/constants/index.ts
@@ -1,4 +1,5 @@
 export const CARS_PER_PAGE = 7;
+export const WINNERS_PER_PAGE = 10;
 export const TOTAL_RANDOM_CARS = 100;
 
 export const DEFAULT_CAR_COLOR = "#ffff00";
@@ -12,4 +13,14 @@ export const API_SETTINGS = {
     WINNERS: "winners",
   },
   BASE_URL: "http://127.0.0.1",
+};
+
+export const HTTP_STATUSES = {
+  OK_200: 200,
+  CREATE_201: 201,
+  NO_CONTENT_204: 204,
+  BAD_REQUEST_400: 400,
+  NOT_AUTHORIZED_401: 401,
+  NOT_FOUND_404: 404,
+  INTERNAL_SERVER_ERROR_500: 500,
 };
