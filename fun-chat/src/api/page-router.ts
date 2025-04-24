04d93e01caa8e76b5119f43956244c6e0f3e9a7f
feafania <173483105+feafania@users.noreply.github.com>
2025-04-24 22:57:27 +0100

    feat: add page router

---
+++ fun-chat/src/api/page-router.ts
@@ -0,0 +1,61 @@
+import { PageType } from "../types/enum";
+import { getState } from "../utils/helpers";
+import { renderLoginView } from "../views/pages/login/login";
+import { renderMainView } from "../views/pages/chat/chat";
+import { renderAboutView } from "../views/pages/about/about";
+import Header from "../views/common/sections/header/header";
+
+const PageRouter = {
+  currentPage: PageType.login as PageType,
+  init() {
+    globalThis.addEventListener("popstate", () => this.handleRoute());
+    this.handleRoute();
+  },
+
+  async handleRoute() {
+    const rawPath = location.pathname.slice(1);
+
+    const isValidPage = (Object.values(PageType) as string[]).includes(rawPath);
+    const path = isValidPage ? (rawPath as PageType) : undefined;
+
+    let page: PageType;
+
+    if (path === PageType.about) {
+      page = path;
+    } else {
+      const isLoggedIn = getState("token");
+      page = isLoggedIn ? PageType.chat : PageType.login;
+      if (!path || path !== page) {
+        history.replaceState(undefined, "", `/${page}`);
+      }
+    }
+
+    this.currentPage = page;
+
+    const renderMap = {
+      [PageType.login]: renderLoginView,
+      [PageType.chat]: renderMainView,
+      [PageType.about]: renderAboutView,
+    };
+
+    const render = renderMap[page];
+
+    if (render) {
+      const element = await render();
+      const container = document.querySelector("#view-container");
+      if (container) {
+        container.innerHTML = "";
+        container.append(element);
+      }
+    }
+
+    Header.replaceNav();
+  },
+
+  navigateTo(path: PageType) {
+    history.pushState(undefined, "", `/${path}`);
+    this.handleRoute();
+  },
+};
+
+export default PageRouter;
