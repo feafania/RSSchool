f178a6df4345c4aa987ecdfe3de7a73bc56e889b
feafania <173483105+feafania@users.noreply.github.com>
2025-04-24 22:55:02 +0100

    feat: add entry point with index and init-app

---
+++ fun-chat/src/index.ts
@@ -0,0 +1,7 @@
+import initApp from "./init-app";
+
+const root = document.createElement("div");
+root.id = "root";
+document.body.append(root);
+
+initApp(root);
---
+++ fun-chat/src/init-app.ts
@@ -0,0 +1,16 @@
+import "./global.css";
+import createMain from "./views/common/sections/main/main";
+import createFooter from "./views/common/sections/footer/footer";
+import PageRouter from "./api/page-router";
+import Header from "./views/common/sections/header/header";
+
+export default function initApp(root: HTMLElement) {
+  root.innerHTML = "";
+
+  const header = Header.create();
+  const main = createMain();
+  const footer = createFooter();
+
+  root.append(header, main, footer);
+  PageRouter.init();
+}
