b9299677fbf94bd636abc91e9fef5ea648fd9881
feafania <173483105+feafania@users.noreply.github.com>
2025-04-28 00:51:57 +0100

    feat: add wait function

--- fun-chat/src/utils/helpers.ts
+++ fun-chat/src/utils/helpers.ts
@@ -1,6 +1,4 @@
 import { StorageItem } from "../types/interfaces";
-import { HttpStatusType } from "../types/types";
-import { API_SETTINGS } from "../constants";
 
 export function capitalizeString(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
@@ -26,27 +24,8 @@ export function getLocalState(key: string): string | null {
   return localStorage.getItem(key);
 }
 
-export function ErrorHandler(
-  error: Response | Error,
-  errorCode: HttpStatusType,
-  errorMessage: string,
-  otherMessage: string,
-  handler: (message: string) => void,
-): void {
-  if (error instanceof Response && error.status === errorCode) {
-    handler(errorMessage);
-  } else if (
-    error instanceof Error &&
-    error.message.includes(errorCode.toString())
-  ) {
-    handler(errorMessage);
-  } else {
-    handler(otherMessage);
-  }
-}
-
-export function buildUrl(path: string): string {
-  return `${API_SETTINGS.BASE_URL}:${API_SETTINGS.PORT}/${path}`;
+export async function wait(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export function disableButton(
